1b64777b9fba69637e275a579b43e903e781207d
Olga Mykhailova <100313107+OlgaMykhailova@users.noreply.github.com>
2025-11-13 16:10:00 +0200

    added basic styles

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,8 @@ import type { Metadata } from "next";
 import { Raleway } from "next/font/google";
 import localFont from "next/font/local";
 import "./globals.css";
+import Header from "@/components/shared/header/Header";
+import Footer from "@/components/shared/footer/Footer";
 
 const raleway = Raleway({
   variable: "--font-raleway",
@@ -36,8 +38,12 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="uk">
-      <body className={`${raleway.variable} ${azbuka.variable} antialiased`}>
-        {children}
+      <body
+        className={`${raleway.variable} ${azbuka.variable} flex min-h-screen flex-col text-[16px] font-normal leading-[120%] antialiased`}
+      >
+        <Header />
+        <main className="flex-1"> {children}</main>
+        <Footer />
       </body>
     </html>
   );
