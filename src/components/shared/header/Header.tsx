66bc61ffa041569631cb866da9496bdc1735077e
Shade <kulakaa073@gmail.com>
2025-11-13 19:15:45 +0200

    fix: fixing fonts

--- src/components/shared/header/Header.tsx
+++ src/components/shared/header/Header.tsx
@@ -25,7 +25,7 @@ export default function Header() {
                         <Navigation />
                         <StarIcon className="hidden lg:block text-black-light" />
                         <MainButton
-                            className="w-[175px] h-[43px] lg:w-[219px] lg:h-13.5 text-[10px] leading-[12px] lg:text-[12px] lg:leading-[14px] font-azbuka04 uppercase"
+                            className="w-[175px] h-[43px] lg:w-[219px] lg:h-13.5 text-[10px] leading-[12px] lg:text-[12px] lg:leading-[14px] font-azbuka uppercase"
                             variant="outline"
                         >
                             Забронювати відвідування
