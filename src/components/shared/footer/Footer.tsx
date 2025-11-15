05190e78b3ae8c3103ba3b28034e2555156e080a
Shade <kulakaa073@gmail.com>
2025-11-15 18:03:07 +0200

    fix: footer styles

--- src/components/shared/footer/Footer.tsx
+++ src/components/shared/footer/Footer.tsx
@@ -14,7 +14,14 @@ import SectionTitle from "../titles/SectionTitle";
 export default function Footer() {
     return (
         <footer className="pt-[100px] relative overflow-hidden">
-            <div className="w-full pb-[27px] sm:pb-6 lg:pb-3 border-b border-black">
+            <motion.div
+                variants={fadeInAnimation({ delay: 0.2 })}
+                initial="hidden"
+                whileInView="visible"
+                exit="exit"
+                viewport={{ once: true, amount: 0.1 }}
+                className="w-full pb-[27px] sm:pb-6 lg:pb-3 border-b border-black"
+            >
                 <Container className="sm:max-w-full">
                     <div className="sm:max-w-[592px] lg:max-w-full lg:flex lg:items-center lg:justify-between">
                         <SectionTitle className="mb-4 sm:mb-7 sm:text-[64px]">
@@ -27,9 +34,9 @@ export default function Footer() {
                                 whileInView="visible"
                                 exit="exit"
                                 viewport={{ once: true, amount: 0.1 }}
-                                className="text-purple w-13 h-13 md:w-17.5 md:h-17.5 flex items-center justify-center -ml-2.5 md:ml-0 lg:mr-[78px]"
+                                className="text-purple w-13 h-13 sm:w-17.5 sm:h-17.5 flex items-center justify-center -ml-2.5 md:ml-0 lg:mr-[78px]"
                             >
-                                <StarIcon className="lg:w-17 lg:h-17" />
+                                <StarIcon className="sm:w-17 sm:h-17" />
                             </motion.div>
 
                             <motion.p
@@ -38,22 +45,22 @@ export default function Footer() {
                                 whileInView="visible"
                                 exit="exit"
                                 viewport={{ once: true, amount: 0.1 }}
-                                className="text-[16px] md:text-[24px] leading-[120%] text-right uppercase max-w-[255px] md:max-w-[382px]"
+                                className="text-[16px] sm:text-[24px] leading-[120%] text-right uppercase max-w-[255px] sm:max-w-[382px]"
                             >
                                 — маленьке відкриття, а кожна усмішка — нагорода
                             </motion.p>
                         </div>
                     </div>
                 </Container>
-            </div>
-            <Container className="pt-7 lg:pt-6.5 pb-[23.5px] lg:pb-[29px] pl-[27px] relative">
+            </motion.div>
+            <Container className="pt-7 lg:pt-6.5 pb-[23.5px] lg:pb-[29px] pl-[27px] relative sm:max-w-full">
                 <div className="flex flex-col lg:flex-row sm:items-start sm:justify-between sm:max-w-[592px] mb-[39px] lg:mb-[129px] lg:max-w-full">
                     <SocialsGroup className="mb-[27px] lg:mb-0 sm:hidden lg:flex" />
                     <div className="lg:w-[768px] sm:flex sm:flex-row-reverse sm:justify-between">
                         <Contacts className="mb-[27px] sm:mb-0" />
-                        <div className="sm:w-[148px]">
+                        <div className="sm:w-[148px] sm:mr-[67px] lg:mr-0">
                             <RulesButton className="mb-[27px] lg:mb-[52px]" />
-                            <PublicOfferButton className="lg:ml-[-182px] xl:ml-[-432px]" />
+                            <PublicOfferButton className="lg:ml-[-182px] xl:ml-[-432px] " />
                         </div>
                     </div>
                 </div>
@@ -99,7 +106,7 @@ export default function Footer() {
                     className="flex items-center justify-between"
                 >
                     <Rights />
-                    <SocialsGroup className="hidden sm:block lg:hidden mr-[248px]" />
+                    <SocialsGroup className="hidden sm:block lg:hidden mr-[228px]" />
                 </motion.div>
                 <FooterText className="lg:block hidden" />
             </Container>
