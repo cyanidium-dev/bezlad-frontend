57b018d256e7c315e8cd619818029e87707bb9bd
Olga Mykhailova <100313107+OlgaMykhailova@users.noreply.github.com>
2025-11-15 19:03:45 +0200

    added title

---
+++ src/components/homePage/ctaOrder/CTAOrder.tsx
@@ -0,0 +1,14 @@
+import Container from "@/components/shared/container/Container";
+
+export default function CTAOrder() {
+  return (
+    <section className="rounded-[32px] overflow-hidden bg-purple-ultra-light">
+      <Container className="pt-[46px] pb-[18px]">
+        <h2 className="font-azbuka text-[32px] font-normal leading-[120%] uppercase">
+          Індивідуальний супровід від наших{" "}
+          <span className="text-purple-dark">нянь</span>
+        </h2>
+      </Container>
+    </section>
+  );
+}
