2211961bdef9bd2f19e59b822a48ef0a920ff809
Shade <kulakaa073@gmail.com>
2025-11-16 10:32:05 +0200

    fix: reordering components

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -12,10 +12,9 @@ export default async function HomePage() {
     return (
         <>
             <Hero />
+            <InteractiveZones />
             <CTAOrder />
             <CTAContactUs />
-            <InteractiveZones />
         </>
     );
-
 }
