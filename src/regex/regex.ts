5f89a9d9b184784700652952db056738118b5a06
Olga Mykhailova <100313107+OlgaMykhailova@users.noreply.github.com>
2025-11-17 15:53:23 +0200

    added order form markup

--- src/regex/regex.ts
+++ src/regex/regex.ts
@@ -1,2 +1,9 @@
 export const contactsPhoneRegex =
-    /^\+38\s?\(?(\d{3})\)?\s?(\d{3})\s?(\d{2})\s?(\d{2})$/;
+  /^\+38\s?\(?(\d{3})\)?\s?(\d{3})\s?(\d{2})\s?(\d{2})$/;
+
+export const formPhoneRegex = /^\+38 \(\d{3}\) \d{3}-\d{2}-\d{2}$/;
+
+export const nameRegex = /^[a-zA-Zа-яА-ЯіІїЇґҐєЄ\s'"-]+$/;
+
+export const emailRegex =
+  /^([a-zA-Z0-9]+)([a-zA-Z0-9?'"`#$%&*+_./|^{}~]+)?@([a-zA-Z0-9_\-.]+)([.][a-zA-Z]{2,3})$/;
---
+++ src/schemas/orderFormValidation.ts
@@ -0,0 +1,34 @@
+import * as yup from "yup";
+import { formPhoneRegex, nameRegex, emailRegex } from "../regex/regex";
+
+export const useOrderFormValidation = () => {
+  const callBackFormValidationSchema = yup.object().shape({
+    name: yup
+      .string()
+      .min(2, "Повинно містити від 2 до 30 символів")
+      .max(30, "Повинно містити від 2 до 30 символів")
+      .matches(nameRegex, "Допустимі літери та дефіс, апостроф, лапки")
+      .required("Дане поле є обов'язковим до заповнення"),
+
+    phone: yup
+      .string()
+      .matches(formPhoneRegex, "Вкажіть правильний номер телефону")
+      .test(
+        "first-digit-after-38",
+        "Після +38 має бути цифра 0",
+        (value) => !!value && value.startsWith("+38 (0")
+      )
+      .required("Дане поле є обов'язковим до заповнення"),
+    email: yup
+      .string()
+      .matches(emailRegex, "Введіть валідний email")
+      .required("Дане поле є обов'язковим до заповнення"),
+    message: yup
+      .string()
+      .min(2, "Повинно містити від 2 до 400 символів")
+      .max(400, "Повинно містити від 2 до 400 символів")
+      .required("Дане поле є обов'язковим до заповнення"),
+  });
+
+  return callBackFormValidationSchema;
+};
