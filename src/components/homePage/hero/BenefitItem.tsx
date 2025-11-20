21e53956da178733e9dcc0ce2ae0f293f8e62df5
Olga Mykhailova <100313107+OlgaMykhailova@users.noreply.github.com>
2025-11-20 13:46:25 +0200

    updated styles

--- src/components/homePage/hero/BenefitItem.tsx
+++ src/components/homePage/hero/BenefitItem.tsx
@@ -44,20 +44,22 @@ export default function BenefitItem({ benefit }: BenefitItemProps) {
     <motion.li
       ref={ref}
       variants={listItemVariantsLeft}
-      className="relative -z-10 flex flex-col justify-center items-center w-[207px] rounded-[14px] overflow-hidden"
+      className="relative flex flex-col justify-center items-center w-[207px] rounded-[14px] overflow-hidden"
       style={{
         background:
           "linear-gradient(129.84deg, #000000 27.26%, #FFFFFF 97.59%)",
         padding: "1px",
+        zIndex: 0,
       }}
     >
       <div
-        className="absolute top-[1px] left-[1px] w-[calc(100%-2px)] h-[calc(100%-2px)] items-center rounded-[12.5px] bg-yellow 
-      backdrop-blur-[19px]"
-      />
-      <div
-        className="relative flex flex-col justify-center items-center w-full h-full py-4 px-8 rounded-[12.5px] bg-white/6 
-      shadow-[inset_0px_4px_12px_rgba(255,255,255,0.06)]"
+        className="relative flex flex-col justify-center items-center w-full py-4 px-8 rounded-[13px] 
+      shadow-[inset_0px_4px_12px_rgba(255,255,255,0.06)] backdrop-blur-[19px]"
+        style={{
+          backgroundColor: "#f8ff66",
+          backgroundImage:
+            "linear-gradient(rgba(255, 255, 255, 0.06), rgba(255, 255, 255, 0.06))",
+        }}
       >
         <p className="mb-px font-azbuka text-[64px] font-normal leading-[100%] uppercase">
           <motion.span>{rounded}</motion.span>
